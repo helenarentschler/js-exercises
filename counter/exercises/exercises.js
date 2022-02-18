7fa2405d7ed1b95d50db6bb134b95327fdf70e72
Helena Rentschler <helrentschler@gmail.com>
2022-02-18 14:03:21 -0300

    add basic css styling, create exercises folder

---
+++ counter/exercises/exercises.js
@@ -0,0 +1,41 @@
+// math operations - convert my age into dog years
+    // let myAge = 19
+    // let humaDogRatio = 7
+    // let myDogAge = myAge * humaDogRatio
+    // console.log(myDogAge)
+
+// increment - bonus
+    // let bonusPoints = 50
+    // bonusPoints = bonusPoints + 50
+    // bonusPoints = bonusPoints - 75
+    // bonusPoints = bonusPoints + 45
+    // console.log(bonusPoints)
+
+// functions - race game
+    
+    // function countdown () {
+    //     console.log(5)
+    //     console.log(4)
+    //     console.log(3)
+    //     console.log(2)
+    //     console.log(1)
+    // }
+
+    // // setting up the race:
+    // countdown () //calling fuction
+
+    // // starting again:
+    // countdown ()
+
+// creating a function that logs out the sum of all the lap times
+    // let lap1 = 34
+    // let lap2 = 33
+    // let lap3 = 36
+
+    // function LogLapSum () {
+    //     let sum = lap1 + lap2 + lap3
+    //     console.log(sum)
+    // }
+
+    // LogLapSum ()
+    // console.log(sum) (does not work, let variables are block scoped)
