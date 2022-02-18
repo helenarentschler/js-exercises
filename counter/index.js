70664ec8d9d15bfdbd45e845f9ee857fc97b8a88
Helena Rentschler <helrentschler@gmail.com>
2022-02-18 13:59:30 -0300

    add basic css styling

--- counter/index.js
+++ counter/index.js
@@ -1,5 +1,13 @@
 // document.getElementById("count-el").innerText = 5
 
+// comment a block of code selected at the same time: ctrl + k + c
+
 let count = 0
 
+count = count + 1
+
 console.log(count)
+
+function increment() {
+    console.log("The button was clicked!")
+}
