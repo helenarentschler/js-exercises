19fd26cdc1e767a0bcd42fe6cf44a11105adb144
Helena Rentschler <helrentschler@gmail.com>
2022-02-28 21:21:15 -0300

    push and fetch localStorage data onclick

--- extension/leads-tracker/index.js
+++ extension/leads-tracker/index.js
@@ -3,10 +3,16 @@ const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 let myLeads = []
 
+localStorage.clear()
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    inputEl.value = ''
+    inputEl.value = '' 
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+    console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLeads() {
