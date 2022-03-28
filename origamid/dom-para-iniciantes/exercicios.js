60a46b9a07346429ac408bdfb6ee85eae6e42f14
Helena Rentschler <helrentschler@gmail.com>
2022-03-28 20:59:27 -0300

    inicio DOM

---
+++ origamid/dom-para-iniciantes/exercicios.js
@@ -0,0 +1,39 @@
+//DOM
+    //window.alert('Olá!')
+    //alert('Tudo bem?'); // funciona, pois objeto window é global
+    // window e doument: objetos pais do DOM
+        
+        const href = window.location.href;
+        console.log(href);
+
+        const h1Selecionado = document.querySelector('h1');
+        console.log(h1Selecionado);
+
+    //toda tag html herda propriedades de um objeto Element
+        console.log(h1Selecionado.classList); // retorna uma array-like
+        console.log(h1Selecionado.classList[0]);
+
+        const h1Classes = h1Selecionado.classList;
+
+        h1Selecionado.addEventListener('click', function() {
+            console.log('Clicou em ', h1Selecionado.innerText);
+        });
+    
+    // Exercicios
+    // Retorne o url da página atual utilizando o objeto window
+        const url = window.location.href;
+        console.log(href);
+
+    // Seleciona o primeiro elemento da página que possua a classe ativo
+        const elAtivo = document.querySelector('.ativo');
+        console.log(elAtivo);
+
+    // Retorne a linguagem do navegador
+        const linguagem = navigator.language;
+        console.log(linguagem);
+
+    // Retorne a largura da janela 
+        const larguraJanela = window.innerWidth;
+        console.log(larguraJanela);
+
+        
